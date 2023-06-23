6c90970cbebf801628233d57d109e717e1606e45
alisondraV <alice.7414122013@gmail.com>
2023-06-23 17:23:56 -0400

    Update the main page and add a profile page

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,9 +1,22 @@
-@extends('layouts.base')
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
-@section('body')
-    @yield('content')
-    
-    @isset($slot)
-        {{ $slot }}
-    @endisset
-@endsection
+    <title>TALL Profile</title>
+
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+
+    @livewireStyles
+</head>
+<body>
+<div class="min-h-screen bg-gray-100 dark:bg-gray-900">
+    <main>
+        <h1>Welcome to the TALL Profile!</h1>
+    </main>
+</div>
+@livewireScripts
+</body>
+</html>
